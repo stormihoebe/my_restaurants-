9d764f3c4f6841ab4f3040508280f9445c90bb95
Riley Watts and Stormi Hoebelheinrich <riley+stormi@>
2017-05-22 13:47:15 -0700

    add new font and use new font to style app title

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -1,15 +1,18 @@
 package com.epicodus.myrestaurants;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mAppNameTextView;
     private Button mFindRestaurantsButton;
     private static final String TAG = MainActivity.class.getSimpleName();
     private EditText mLocationEditText;
@@ -18,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+        Typeface living = Typeface.createFromAsset(getAssets(), "fonts/living.ttf");
+        mAppNameTextView.setTypeface(living);
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
