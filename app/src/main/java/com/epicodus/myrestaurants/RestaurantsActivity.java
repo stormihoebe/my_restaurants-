d90c17700d3ba95687dded14e282f5027371eb24
Riley Watts and Stormi Hoebelheinrich <riley+stormi@>
2017-05-22 15:01:20 -0700

    add list to restaurants page

--- app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/RestaurantsActivity.java
@@ -3,10 +3,18 @@ package com.epicodus.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class RestaurantsActivity extends AppCompatActivity {
+        private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
         private TextView mLocationTextView;
+        private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +22,9 @@ public class RestaurantsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_restaurants);
 
         mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        mListView = (ListView) findViewById(R.id.listView);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, restaurants);
+        mListView.setAdapter(adapter);
 
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
